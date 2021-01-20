07eef06e6300ae40a410b03363009c49e6fcb2b3
Nathan Pham <nathanpham.me@gmail.com>
2021-01-19 23:27:40 -0800

    initial tempaltes

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/')
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasklist-python.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 db = SQLAlchemy(app)
+
+db.create_all()
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,6 +19,11 @@ class Task(db.Model):
 
     def __repr__(self):
         return 'Task(content=%s)' % self.content        
+
+@app.route('/')
+def task_list():
+    tasks = Task.query.all()
+    return render_template("list.html", tasks=tasks)
 
 if __name__ == "__main__":
     app.run(debug=True)
